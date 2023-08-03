b4e4b0d8c2c65c68ba7d130730570c0b19c156aa
Nguyễn Công Hậu <111746781+nguyenhau8209@users.noreply.github.com>
2023-08-03 22:34:15 +0700

    update chuc nang gui tin nhan qua lai giua server va client by Hau

--- app/src/main/java/com/example/socket_client/MainActivity.java
+++ app/src/main/java/com/example/socket_client/MainActivity.java
@@ -1,34 +1,45 @@
 package com.example.socket_client;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvReceivedData;
-    private EditText etServerName, etServerPort;
+    private TextView tvReceivedData, tvShowMessage;
+    private EditText etServerName, etServerPort, etSendMessageToServer;
     private Button buttonClientConnect;
     private String serverName;
     private int serverPort;
+    Socket socket;
+    private Boolean connect = false;
+    BufferedReader br_input;
+    private PrintWriter output_writer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvReceivedData = findViewById(R.id.tvMessageClient);
+        tvReceivedData = findViewById(R.id.tvReceivedData);
+        tvShowMessage = findViewById(R.id.tvshowtinnhan);
         etServerName = findViewById(R.id.edtServerIP);
         etServerPort = findViewById(R.id.edtServerPort);
         buttonClientConnect = findViewById(R.id.btnConnect);
+        etSendMessageToServer = findViewById(R.id.etSendMessageToServer);
     }//on create
 
     public void onClickConnect(View view) {
@@ -39,24 +50,63 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Socket socket = new Socket(serverName, serverPort);
+                    socket = new Socket(serverName, serverPort);
 
 
-                    BufferedReader br_input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String txtFromServer = br_input.readLine();
-
+                    br_input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    output_writer = new PrintWriter(socket.getOutputStream(), true);
+                    // Cập nhật giao diện khi kết nối thành công
+//
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            tvReceivedData.setText(txtFromServer);
+                            tvReceivedData.setText("Connected to Server");
                         }
                     });
+
+                    //nhan tin nhan tu server va hien thi len giao dien
+                    String messageFromServer;
+                    while ((messageFromServer = br_input.readLine()) != null) {
+                        final String messageFinal = messageFromServer;
+                        Log.d("TAG", "run: " + messageFinal);
+//
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvShowMessage.setText("Message from server: " + messageFinal);
+                            }
+                        });
+                    }
+
+                    socket.close();
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    e.printStackTrace();
                 }
             }
         }).start();
-    }
+    }//onClickConnect
+
+    public void onClickSendMessageToServer(View view) {
+        String messageToSend = etSendMessageToServer.getText().toString();
+        if (!messageToSend.isEmpty()) {
+            sendMessageToServer(messageToSend);
+            Toast.makeText(this, "Send message to server successfully", Toast.LENGTH_SHORT).show();
+            etSendMessageToServer.setText("");
+        } else {
+            Toast.makeText(this, "Send message to server false", Toast.LENGTH_SHORT).show();
+        }
+    }//onclick sendmessagetoserver
+
+    public void sendMessageToServer(String message) {
+        if (output_writer != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    output_writer.println(message);
+                }
+            }).start();
+        }
+    }//sendMessageToServer
 
 
 }
--- myapplicationserver/src/main/java/com/example/myapplicationserver/MainActivityServer.java
+++ myapplicationserver/src/main/java/com/example/myapplicationserver/MainActivityServer.java
@@ -3,18 +3,31 @@ package com.example.myapplicationserver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class MainActivityServer extends AppCompatActivity {
-    private TextView tvServerName, tvServerPort, tvStatus;
+    private TextView tvServerName, tvServerPort, tvStatus, tvMessageFromClient;
+    private EditText etMessage;
+    private ArrayList<Socket> connectedSockets; // Danh sách các Socket đã kết nối
     private String serverIP = "10.0.2.16"; //check your own IP address in the emulator or real device
     private int serverPort = 1234; // choose your own port number, >1023, avoid reserved ones, like 8080...
+
+    private ServerThread serverThread;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +37,37 @@ public class MainActivityServer extends AppCompatActivity {
         tvServerName = findViewById(R.id.tvServerIP);
         tvServerPort = findViewById(R.id.tvServerPort);
         tvStatus = findViewById(R.id.tvStatusServer);
+        etMessage = findViewById(R.id.etMessage);
+        tvMessageFromClient = findViewById(R.id.tvMessageFromClient);
 
         tvServerName.setText(serverIP);
         tvServerPort.setText(String.valueOf(serverPort));
     }//on create
 
-    private ServerThread serverThread;
 
-    public void onClickStartServer (View view){
+    public void onClickStartServer(View view) {
         serverThread = new ServerThread();
+        Toast.makeText(this, "Server started", Toast.LENGTH_SHORT).show();
         serverThread.startServer();
     }
 
-    public void onClickStopServer (View view){
+    public void onClickStopServer(View view) {
 
         serverThread.stopServer();
+        Toast.makeText(this, "server stopped", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSend(View view) {
+        //Lay noi dung tu edittext
+        String messageToSend = etMessage.getText().toString();
+        //gui noi dung toi tat ca client da ket noi
+        if (serverThread != null && !messageToSend.isEmpty()) {
+            serverThread.sendMessageToAllClient(messageToSend);
+            Toast.makeText(this, "Send message to client successfully", Toast.LENGTH_SHORT).show();
+            etMessage.setText("");
+        } else {
+            Toast.makeText(this, "Send message to client false", Toast.LENGTH_SHORT).show();
+        }
     }
 
     class ServerThread extends Thread implements Runnable {
@@ -51,36 +80,6 @@ public class MainActivityServer extends AppCompatActivity {
             start();
         }
 
-        @Override
-        public void run() {
-            try {
-                serverSocket = new ServerSocket(serverPort);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        tvStatus.setText("Waiting for Clients");
-                    }
-                });
-
-                while (serverRunning) {
-                    Socket socket = serverSocket.accept();
-                    count++;
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tvStatus.setText("Connected to " + socket.getInetAddress() + " : " + socket.getLocalPort());
-                        }
-                    });
-
-                    PrintWriter outputServer = new PrintWriter(socket.getOutputStream());
-                    outputServer.write("Welcome to server: " + count);
-                    outputServer.flush();
-                    socket.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
 
         public void stopServer() {
             serverRunning = false;
@@ -90,6 +89,10 @@ public class MainActivityServer extends AppCompatActivity {
                     if (serverSocket != null) {
                         try {
                             serverSocket.close();
+//                            for (Socket socket : connectedSockets) {
+//                                socket.close(); // Đóng tất cả các Socket đã kết nối
+//                            }
+//                            connectedSockets.clear(); // Xóa danh sách các Socket đã kết nối
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -97,12 +100,101 @@ public class MainActivityServer extends AppCompatActivity {
                                 }
                             });
                         } catch (IOException e) {
-                            throw new RuntimeException(e);
+                            e.printStackTrace();
                         }
                     }
                 }
             }).start();
         }//stopserver
 
+        public void sendMessageToAllClient(String message) {
+            if (serverSocket != null) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (ConnectedSockets connectedSockets1 : clientsList) {
+                            connectedSockets1.sendMessageToClient(message);
+                        }
+                    }
+                }).start();
+            }
+        }//sendmessage
+
+        // mảng các thiết bị đã kết nối
+        private ArrayList<ConnectedSockets> clientsList = new ArrayList<>();
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(serverPort);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvStatus.setText("Waiting for Clients");
+                    }
+                });
+
+                while (serverRunning) {
+                    Socket socket = serverSocket.accept();
+                    ConnectedSockets client = new ConnectedSockets(socket);
+                    client.start();
+                    clientsList.add(client);
+                    count++;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvStatus.setText("Connected to " + socket.getInetAddress() + " : " + socket.getLocalPort());
+                        }
+                    });
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        class ConnectedSockets extends Thread {
+            private Socket clientSocket;
+            private BufferedReader br_input;
+            private PrintWriter output_Client;
+
+            public ConnectedSockets(Socket socket) {
+                clientSocket = socket;
+                try {
+                    // Lấy luồng đầu vào và luồng đầu ra của Client
+                    br_input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    output_Client = new PrintWriter(clientSocket.getOutputStream(), true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Phương thức gửi tin nhắn đến Client
+            public void sendMessageToClient(String message) {
+                output_Client.println(message);
+            }
+
+            @Override
+            public void run() {
+                try {
+                    // Hiển thị tin nhắn từ Client lên giao diện
+                    String messageFromClient;
+                    while ((messageFromClient = br_input.readLine()) != null) {
+                        // Hiển thị tin nhắn từ Client lên giao diện
+                        final String finalMessage = messageFromClient;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvMessageFromClient.setText("Tin nhắn từ client: " + finalMessage);
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }//class connected socket
     }//serverthread
 }//main
